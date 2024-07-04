804b89a9a507c42c11aa54d51e553eba1d57e9b6
Re9iNee <attarzadeh76@gmail.com>
2024-07-05 00:14:21 +0330

    Routing - 2nd Video

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <Link href={"/"}>Homepage</Link>
+      <h1>This is an About page 🍩</h1>
+      <h3>In about page im going to demonstrate whats going on this website</h3>
+    </div>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <Link href={"/"}>Homepage</Link>
+      <h1>Here is the contact page you can connect with me here 😊</h1>
+    </div>
+  );
+}
